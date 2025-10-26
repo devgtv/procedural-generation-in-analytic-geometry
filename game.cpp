5f2f1d4f25cf6f29e7336cb155a7057088a5ba6c
devgtv <gustavobarrosfialho1402gb@gmail.com>
2025-10-26 15:44:15 -0300

    agora ta funcionando

--- game.cpp
+++ game.cpp
@@ -4,7 +4,6 @@
 #include <ctime>
 #include <windows.h>
 #include <limits>
-#include <random>
 #include <cmath>
 using namespace std;
 
@@ -25,43 +24,39 @@ bool desafioVetores() {
     cout << "B = (" << B.x << "," << B.y << "," << B.z << ")\n";
 
     // Escolhe operação aleatória (0 = subtração, 1 = soma, 2 = módulo)
-    random_device rd;
-    mt19937 gen(rd());
-    uniform_int_distribution<> distrib(0, 2); // define o intervalo no qual os numeros podem ser gerados
-    int operacao = distrib(gen); // gera um numero aleatorio entre 0 e 2
-    // Mostra qual operação o jogador deve calcular
-        int rx = 0, ry = 0, rz = 0;
-        double rm = 0.0;
-        
-     
-        bool correto;
-        if (operacao == 1) { 
-            cout << "Calcule A + B = (?, ?, ?)\n";
-            cout << "Digite o valor de x: ";
-            cin >> rx;
-            cout << "Digite o valor de y: ";
-            cin >> ry;
-            cout << "Digite o valor de z: ";
-            cin >> rz;
-            correto = (rx == A.x + B.x && ry == A.y + B.y && rz == A.z + B.z);
-        } else if (operacao == 0) { 
-            cout << "Calcule A - B = (?, ?, ?)\n";
-            cout << "Digite o valor de x: ";
-            cin >> rx;
-            cout << "Digite o valor de y: ";
-            cin >> ry;
-            cout << "Digite o valor de z: ";
-            cin >> rz;
-            correto = (rx == A.x - B.x && ry == A.y - B.y && rz == A.z - B.z);
-        } else { 
-            cout << "Calcule |A + B| = ?\n";
-            cout << "Digite o valor do modulo: ";
-            cin >> rm;
-            double m = sqrt((A.x + B.x) * (A.x + B.x) +
-                              (A.y + B.y) * (A.y + B.y) +
-                              (A.z + B.z) * (A.z + B.z));
-            correto = (fabs(rm - m) < 0.1); // Tolerância de uma casa decimal
-        }
+    int operacao = rand() % 3; 
+
+    int rx = 0, ry = 0, rz = 0;
+    double rm = 0.0;
+    bool correto;
+
+    if (operacao == 1) { 
+        cout << "Calcule A + B = (?, ?, ?)\n";
+        cout << "Digite o valor de x: ";
+        cin >> rx;
+        cout << "Digite o valor de y: ";
+        cin >> ry;
+        cout << "Digite o valor de z: ";
+        cin >> rz;
+        correto = (rx == A.x + B.x && ry == A.y + B.y && rz == A.z + B.z);
+    } else if (operacao == 0) { 
+        cout << "Calcule A - B = (?, ?, ?)\n";
+        cout << "Digite o valor de x: ";
+        cin >> rx;
+        cout << "Digite o valor de y: ";
+        cin >> ry;
+        cout << "Digite o valor de z: ";
+        cin >> rz;
+        correto = (rx == A.x - B.x && ry == A.y - B.y && rz == A.z - B.z);
+    } else { 
+        cout << "Calcule |A + B| = ?\n";
+        cout << "Digite o valor do módulo: ";
+        cin >> rm;
+        double m = sqrt((A.x + B.x) * (A.x + B.x) +
+                        (A.y + B.y) * (A.y + B.y) +
+                        (A.z + B.z) * (A.z + B.z));
+        correto = (fabs(rm - m) < 0.1); // tolerância de uma casa decimal
+    }
 
     // Mostra resultado, espera 2s e limpa tela
     if (correto)
@@ -69,15 +64,13 @@ bool desafioVetores() {
     else
         cout << "ERRADO!\n";
 
-    Sleep(2000); // espera 2 segundos
-    system("cls"); 
+    Sleep(2000);
+    system("cls");
 
     return correto;
 }
 
-
-
-void mostraLabirinto(const vector<vector<char>> &lab, int px, int py) { // imprime o labirinto na tela do jogador
+void mostraLabirinto(const vector<vector<char>> &lab, int px, int py) {
     for (int i = 0; i < lab.size(); i++) {
         for (int j = 0; j < lab[i].size(); j++) {
             if (i == px && j == py)
@@ -90,9 +83,9 @@ void mostraLabirinto(const vector<vector<char>> &lab, int px, int py) { // impri
 }
 
 int main() {
-    SetConsoleOutputCP(65001); // padrão UTF-8
-    srand(time(0)); // executo o padrao de randomizacao comforme o horario do computador da pessoa;
-     
+    SetConsoleOutputCP(65001);
+    srand(time(0)); // inicializa o rand() com base no tempo atual
+
     vector<vector<char>> lab = {
         {'E',' ','X','D',' '},
         {'X',' ','X',' ',' '},
@@ -101,7 +94,7 @@ int main() {
         {' ',' ','D',' ','S'}
     };
 
-    int px = 0, py = 0; // posicao x posicao y
+    int px = 0, py = 0;
     int tentativas = 3;
 
     while (true) {
@@ -114,37 +107,37 @@ int main() {
 
         char move;
         cin >> move;
-        cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // limpa o buffer
+        cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 
         int nx = px, ny = py;
 
-        if (move == 'W' || move == 'w') nx--; // para cima
-        else if (move == 'S' || move == 's') nx++; // para baixo
-        else if (move == 'A' || move == 'a') ny--; // para esquerda
-        else if (move == 'D' || move == 'd') ny++; // para direita 
+        if (move == 'W' || move == 'w') nx--;
+        else if (move == 'S' || move == 's') nx++;
+        else if (move == 'A' || move == 'a') ny--;
+        else if (move == 'D' || move == 'd') ny++;
         else {
             cout << "Comando inválido!" << endl;
             Sleep(2000);
             continue;
         }
 
-        if (nx < 0 || ny < 0 || nx >= lab.size() || ny >= lab[0].size()) { // verrifica se o movimento e invalido
+        if (nx < 0 || ny < 0 || nx >= lab.size() || ny >= lab[0].size()) {
             cout << "Movimento inválido (fora do mapa)!" << endl;
             Sleep(2000);
             continue;
         }
 
         if (lab[nx][ny] == 'X') {
-            cout << "PAREDE! Escolha outro caminho." << endl; // verrifica se e a uma parede
+            cout << "PAREDE! Escolha outro caminho." << endl;
             Sleep(2000);
             continue;
         }
 
-        if (lab[nx][ny] == 'D') { // chama um desafio
+        if (lab[nx][ny] == 'D') {
             bool acerto = desafioVetores();
             if (!acerto) {
                 tentativas--;
-                if (tentativas == 0) { // verrifica se ele perdeu o desdafio e se ainda vai poder continuar no game
+                if (tentativas == 0) {
                     cout << "\nVocê perdeu todas as tentativas. FIM DE JOGO.\n";
                     Sleep(3000);
                     break;
